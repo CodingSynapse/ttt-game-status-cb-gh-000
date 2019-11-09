e5df22f0a169e41d3030ae47bd1857e8af1d034c
Micah ortega <codingsynapse@gmail.com>
2019-11-09 06:58:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,8 @@ def won?(board)
 
       if position_1 == "X" && position_2 =="X" && position_3 == "X" || "O" && position_2 =="O" && position_3 == "O"
         return index
+      elsif draw?(board) == true
+        return false
       end
     end
     return false
